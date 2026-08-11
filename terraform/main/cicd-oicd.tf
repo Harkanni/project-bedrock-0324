1f4ecc4eb809bdddbff6eb1f20dbde89f937ff3f
harkanni <harkanzyempire001@gmail.com>
2026-08-11 17:18:26 +0100

    fix: Simplify OIDC role assumption for GitHub Actions by using wildcard for repository references

--- terraform/main/cicd-oicd.tf
+++ terraform/main/cicd-oicd.tf
@@ -33,10 +33,7 @@ resource "aws_iam_role" "github_actions_role" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = [
-              "repo:Harkanni/project-bedrock-0324:ref:refs/heads/main",
-              "repo:Harkanni/project-bedrock-0324:pull_request"
-            ]
+            "token.actions.githubusercontent.com:sub" = "repo:Harkanni/project-bedrock-0324:*"
           }
         }
       }
